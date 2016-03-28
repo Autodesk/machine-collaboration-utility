0b1065ee587b979644e176fecc146fa15a19ca28
hovanem <matt.hovanec@autodesk.com>
2016-03-28 19:08:25 -0300

    cleaned up koaApp

--- src/app/koaApp.js
+++ src/app/koaApp.js
@@ -1,29 +1,26 @@
-const co = require('co');
-const Koa = require('koa');
-const cors = require('koa-cors');
-const convert = require('koa-convert');
-const bodyparser = require('koa-bodyparser');
-const json = require('koa-json');
-const winston = require('winston');
-const serve = require('koa-static');
-const views = require('koa-views');
-const IO = require('koa-socket');
-const path = require('path');
-const Sequelize = require('sequelize');
-const Promise = require(`bluebird`);
+const co = require(`co`);
+const Koa = require(`koa`);
+const cors = require(`koa-cors`);
+const convert = require(`koa-convert`);
+const bodyparser = require(`koa-bodyparser`);
+const json = require(`koa-json`);
+const winston = require(`winston`);
+const serve = require(`koa-static`);
+const views = require(`koa-views`);
+const IO = require(`koa-socket`);
+const path = require(`path`);
+const Sequelize = require(`sequelize`);
 
 // Import custom middleware libraries
-const Files = require('./middleware/files');
-const Jobs = require('./middleware/jobs');
-const Bots = require('./middleware/bots');
-const Conductor = require('./middleware/conductor');
-const UI = require('./middleware/ui');
+const Files = require(`./middleware/files`);
+const Jobs = require(`./middleware/jobs`);
+const Bots = require(`./middleware/bots`);
+const UI = require(`./middleware/ui`);
 
 class KoaApp {
-  constructor(config, conducting) {
+  constructor(config) {
     this.app = new Koa();
     this.app.context.config = config;
-    this.conducting = conducting;
     this.apiVersion = config.apiVersion;
   }
 
@@ -31,7 +28,7 @@ class KoaApp {
     // Setup logger
     const filename = path.join(__dirname, `../../${this.app.context.config.logFileName}`);
     this.app.context.logger = new (winston.Logger)({
-      level: 'debug',
+      level: `debug`,
       transports: [
         new (winston.transports.Console)(),
         new (winston.transports.File)({ filename }),
@@ -42,12 +39,12 @@ class KoaApp {
     this.app.use(convert(cors()));
     this.app.use(convert(bodyparser()));
     this.app.use(convert(json()));
-    this.app.use(convert(serve(path.join(__dirname, '../client'))));
+    this.app.use(convert(serve(path.join(__dirname, `../client`))));
 
     // set jade render path
-    this.app.use(convert(views(path.join(__dirname, './views'), {
-      root: path.join(__dirname, './views'),
-      default: 'jade',
+    this.app.use(convert(views(path.join(__dirname, `./views`), {
+      root: path.join(__dirname, `./views`),
+      default: `jade`,
     })));
 
     // set ctx function for rendering jade
@@ -66,7 +63,9 @@ class KoaApp {
     // check database connection
     sequelize.authenticate().then((err) => {
       if (err) {
-        this.app.context.logger.error('Unable to connect to the database:', err);
+        const errorMessage = `Unable to connect to the database: ${err}`;
+        this.app.context.logger.error(err);
+        throw (errorMessage);
       } else {
         this.app.context.db = sequelize;
       }
@@ -77,29 +76,19 @@ class KoaApp {
       await files.initialize();
 
       const jobs = new Jobs(this.app, `/${this.apiVersion}/jobs`);
-      console.log(`/${this.apiVersion}/jobs`);
       await jobs.initialize();
 
       const bots = new Bots(this.app, `/${this.apiVersion}/bots`);
-      bots.initialize();
+      await bots.initialize();
 
-      // if (this.conducting) {
-      //   const conductor = new Conductor(this.app, `/${this.apiVersion}/conductor`);
-      //   await conductor.initialize();
-      // } else {
-      // 
-      //   // const bot = new Bot(this.app, `/${this.apiVersion}/bot`, process.env.EXTERNAL_ENDPOINT);
-      //   // await bot.initialize();
-      // }
-      // 
       const ui = new UI(this.app, ``);
       await ui.initialize();
 
-      this.app.context.logger.info('Hydraprint has been initialized successfully.');
+      this.app.context.logger.info(`Hydra-Print has been initialized successfully.`);
     });
 
-    this.app.on('error', (err, ctx) => {
-      this.app.context.logger.error('server error', err, ctx);
+    this.app.on(`error`, (err, ctx) => {
+      this.app.context.logger.error(`server error`, err, ctx);
     });
   }
 }
