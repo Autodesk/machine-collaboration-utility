05f13032d1b0b2bd12ca1d65d21d6571133dcf62
hovanem <matt.hovanec@autodesk.com>
2016-04-01 17:55:22 -0700

    added comments to the usb code

--- src/middleware/bots/server/discovery/usb.js
+++ src/middleware/bots/server/discovery/usb.js
@@ -16,8 +16,10 @@ class UsbDiscovery {
       // Need to wait arbitrary amount of time for Serialport list to update
       await Promise.delay(100);
       SerialPort.list((err, ports) => {
+        // Compare every available port against every known port
         for (let i = 0; i < ports.length; i++) {
           const port = ports[i];
+          // If the port isn't on our list of known ports, then we need to add it
           if (self.ports[port.comName] === undefined) {
             self.ports[port.comName] = port;
             self.detectPort(port);
@@ -30,11 +32,15 @@ class UsbDiscovery {
       // Need to wait arbitrary amount of time for Serialport list to update
       await Promise.delay(100);
       SerialPort.list((err, ports) => {
+        // Go through every known port
         for (const listedPort in self.ports) {
           if (self.ports.hasOwnProperty(listedPort)) {
             const foundPort = _.find(ports, (port) => {
               return port.comName === listedPort;
             });
+            // if one of the ports from our list isn't in the list of available ports
+            // we know that that port was removed
+            // now do all the steps to remove it
             if (foundPort === undefined) {
               const botName = self.sanitizePortName(listedPort);
               const removedBot = self.app.context.bots.bots[botName];
@@ -49,9 +55,14 @@ class UsbDiscovery {
       });
     });
 
+    // Scan through all known serial ports and check if any of them are bots
     SerialPort.list((err, ports) => {
       for (let i = 0; i < ports.length; i++) {
         const port = ports[i];
+        // Add each known serial port to the list
+        // Even if we don't want to use a ports, we need to add it to a list of known ports
+        // so that when a port is unplugged later, we can reference the list of known ports
+        // against the list of available ports, and determine which port was unplugged
         self.ports[port.comName] = port;
         self.detectPort(port);
       }
