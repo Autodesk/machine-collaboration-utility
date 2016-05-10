c505fa6732612e633834e77d33d6ec96c4750dd3
hovanem <matt.hovanec@autodesk.com>
2016-05-09 17:16:21 -0700

    added conductor connect logic. removed decection state from conductor

--- src/server/middleware/conductor/index.js
+++ src/server/middleware/conductor/index.js
@@ -1,12 +1,12 @@
 const router = require(`koa-router`)();
 const Promise = require(`bluebird`);
 const StateMachine = Promise.promisifyAll(require(`javascript-state-machine`));
-const fs = require(`fs`);
-const _ = require(`underscore`);
+
 const request = require(`request-promise`);
 const ip = require(`ip`);
 
 const conductorRoutes = require(`./routes`);
+const Bot = require(`../bots/bot`);
 
 /**
  * This is a Conductor class representing a system for managing multiple pieces of hardware
@@ -30,13 +30,13 @@ class Conductor {
     this.routeEndpoint = routeEndpoint;
     this.router = router;
 
-    this.players = this.app.context.config.conductor.players;
+    this.players = {};
 
     // File reading assets
     this.currentJob = undefined;
 
     this.fsm = StateMachine.create({
-      initial: 'unavailable',
+      initial: 'ready',
       error: (one, two) => {
         const errorMessage = `Invalid Conductor state change action "${one}". State at "${two}".`;
         this.logger.error(errorMessage);
@@ -44,9 +44,6 @@ class Conductor {
       },
       events: [
         /* eslint-disable no-multi-spaces */
-        { name: 'detect',              from: 'unavailable',      to: 'detecting'        },
-        { name: 'detectFail',          from: 'detecting',        to: 'unavailable'      },
-        { name: 'detectDone',          from: 'detecting',        to: 'ready'            },
         { name: 'connect',             from: 'ready',            to: 'connecting'       },
         { name: 'connectFail',         from: 'connecting',       to: 'ready'            },
         { name: 'connectDone',         from: 'connecting',       to: 'connected'        },
@@ -84,7 +81,6 @@ class Conductor {
     try {
       await this.setupRouter();
       await this.setupConductorArms();
-      await this.setupPlayersScanner();
       this.logger.info(`Conductor instance initialized`);
     } catch (ex) {
       this.logger.error(`Conductor initialization error`, ex);
@@ -136,6 +132,7 @@ class Conductor {
     const filesApp = self.app.context.files;
     const theFile = filesApp.getFile(job.fileUuid);
     const filePath = filesApp.getFilePath(theFile);
+    
     // read the json metajob .... etc...
     await self.fsm.startDone();
   }
@@ -190,7 +187,7 @@ class Conductor {
 
   async connect() {
     this.fsm.connect();
-    // this.connectAllPlayers();
+    this.connectAllPlayers();
   }
 
   async disconnect() {
@@ -203,98 +200,88 @@ class Conductor {
     }
   }
 
-  async setupPlayersScanner() {
-    await this.fsm.detect();
-    const scanInterval = setInterval(async () => {
-      let connected = true;
-      for (let i = 0; i < this.players.length; i++) {
-        const uri = 'http://' + ip.address() + ':' + (9001 + i) + '/v1/bot';
-        const requestParams = {
-          method: `GET`,
-          uri,
-          json: true,
-        };
-        let res;
-        try {
-          res = await request(requestParams);
-        } catch (ex) {
-          res = false;
-        }
-        if (res.data && res.data.state) {
-          // this printer is ready
-        } else {
-          connected = false;
-        }
-      }
-      if (connected) {
-        clearInterval(scanInterval);
-        await this.fsm.detectDone();
-      }
-    }, 1000);
-  }
-
   async connectAllPlayers() {
+    // Check if the players are all connected
+    // If they are not, command them to connect
+    // Keep checking on them until they are all connected
     const scanInterval = setInterval(async () => {
       let connected = true;
       // connect all local players
-      for (let i = 0; i < this.players.length; i++) {
-        const uri = 'http://' + ip.address() + ':' + (9001 + i) + '/v1/bot';
-        const requestParams = {
-          method: `GET`,
-          uri,
-          json: true,
-        };
-        let res;
-        try {
-          res = await request(requestParams);
-        } catch (ex) {
-          res = false;
-        }
-        if (res.data && res.data.state && res.data.state === `connected`) {
-          // this bot is connected
-        } else {
-          connected = false;
-        }
-      }
-
-      // connect all external players
-      for (let i = 0; i < this.players.length; i++) {
-        const uri = `${this.players[i].url}/v1/bot`;
-        const requestParams = {
-          method: `POST`,
-          uri,
-          body: {
-            command: `connect`,
-          },
-          json: true,
-        };
-        let res;
-        try {
-          res = await request(requestParams);
-        } catch (ex) {
-          res = false;
-        }
-        if (res.data && res.data.state && res.data.state === `connected`) {
-          // this printer is ready
-        } else {
-          connected = false;
+      for (const player in this.players) {
+        if (this.players.hasOwnProperty(player)) {
+          const uri = this.players[player].settings.port;
+          const requestParams = {
+            method: `GET`,
+            uri,
+            json: true,
+          };
+          let res;
+          try {
+            res = await request(requestParams);
+          } catch (ex) {
+            res = false;
+          }
+          if (res.data && res.data.state && res.data.state === `connected`) {
+            // this bot is connected
+          } else {
+            if (res.data && res.data.state && res.data.state === `ready`) {
+              const connectParams = {
+                method: `POST`,
+                uri,
+                body: {
+                  command: `connect`,
+                },
+                json: true,
+              };
+              request(connectParams);
+            }
+            connected = false;
+          }
         }
       }
-
       if (connected) {
         clearInterval(scanInterval);
-        await this.fsm.detectDone();
+        await this.fsm.connectDone();
       }
     }, 1000);
   }
 
+  // If the database doesn't yet have printers for the endpoints, create them
   async setupConductorArms() {
-    for (const player of this.players) {
-      // const endpoint = `/${this.app.context.config.apiVersion}/${player.name}`;
-      // console.log('endpoint!', endpoint);
-      // const bot = new Bot(this.app, endpoint);
-      // await bot.initialize();
-      // console.log('player!', player);
+    // Sweet through every player
+    for (const player of this.app.context.config.conductor.players) {
+      // Check if a bot exists with that end point
+      let endpoint;
+      switch (this.app.context.config.conductor.comType) {
+        case 'http':
+          endpoint = `http://escher2${player}.local:9000/v1/bots/usb`;
+          break;
+        default:
+          endpoint = `http://escher2${player}.local`;
+      }
+      const bots = this.app.context.bots.getBots();
+      let uniqueEndpoint = true;
+      for (const bot in bots) {
+        if (bots.hasOwnProperty(bot)) {
+          if (bots[bot].settings.port === endpoint) {
+            this.players[player] = bots[bot];
+            uniqueEndpoint = false;
+            break;
+          }
+        }
+      }
+
+      // If a bot doesn't exist yet with that endpoint, create it
+      if (uniqueEndpoint) {
+        const botSettings = this.app.context.bots.createBot({
+          connectionType: this.app.context.config.conductor.comType,
+          port: endpoint,
+        });
+        const dbBot = await this.app.context.bots.Bot.create(botSettings);
+        const botKey = dbBot.dataValues.id;
+        this.app.context.bots.bots[botKey] = await new Bot(this.app, botSettings);
+        this.players[player] = bots[botKey];
+      }
     }
   }
 }
