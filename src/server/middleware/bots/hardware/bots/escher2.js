3c4a753e48615ba9ca6f04a1184100b667947977
hovanem <matt.hovanec@autodesk.com>
2016-06-28 16:41:42 -0700

    updated hardware. added park and unpark functions to Escher2Conductor

--- src/server/middleware/bots/hardware/bots/escher2.js
+++ src/server/middleware/bots/hardware/bots/escher2.js
@@ -1,10 +1,8 @@
-const DefaultBot = require(`./DefaultBot`);
+const Escher2Conductor = require(`./Escher2Conductor`);
 
-module.exports = class Escher2 extends DefaultBot {
+module.exports = class Escher2 extends Escher2Conductor {
   constructor(app) {
     super(app);
-    this.settings.name = `Escher 2.0`;
-    this.settings.model = `Escher2`;
     this.connectionType = `serial`;
     this.vid = 9025;
     this.pid = 66;
