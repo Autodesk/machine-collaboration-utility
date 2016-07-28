1307c2e1643b3246ed933044623d8756c580c8d8
hovanem <matt.hovanec@autodesk.com>
2016-07-27 18:17:51 -0600

    refactored client jobs to update

--- src/client/react/modules/Jobs/Job.js
+++ src/client/react/modules/Jobs/Job.js
@@ -4,19 +4,26 @@ import request from 'superagent';
 export default class Job extends React.Component {
   constructor(props) {
     super(props);
-    this.state = props.job;
+    this.state = {
+      job: props.job,
+    };
     this.handleClick = this.handleClick.bind(this);
   }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ job: nextProps.job });
+  }
+
   handleClick() {
     request.delete(`/v1/jobs`)
-    .send({ uuid: this.state.uuid })
+    .send({ uuid: this.state.job.uuid })
     .set('Accept', 'application/json')
     .end();
   }
   render() {
     return (<div>
-      <div>Job UUID: {this.state.uuid}</div>
-      <div>Job State: {this.state.state}</div>
+      <div>Job UUID: {this.state.job.uuid}</div>
+      <div>Job State: {this.state.job.state}, {this.state.job.percentComplete}</div>
       <button onClick={this.handleClick}>Delete Job</button>
       <br></br>
       <br></br>
--- src/client/react/modules/Jobs/index.js
+++ src/client/react/modules/Jobs/index.js
@@ -5,9 +5,17 @@ import Job from './Job';
 export default class Jobs extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      jobs: this.props.jobs,
+    };
   }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ jobs: nextProps.jobs });
+  }
+
   render() {
-    const jobs = Object.entries(this.props.jobs).map(([jobKey, job]) => {
+    const jobs = Object.entries(this.state.jobs).map(([jobKey, job]) => {
       return <Job key={job.uuid} job={job}/>;
     });
     return (<div>
