623154c695bd8114a9b60cfd9437ef96d3af5f50
matthova <matthova87@gmail.com>
2016-08-11 08:12:13 -0700

    little cleanup

--- server/koaApp.js
+++ server/koaApp.js
@@ -26,6 +26,12 @@ const Files = require(`./middleware/files`);
 const Jobs = require(`./middleware/jobs`);
 const Bots = require(`./middleware/bots`);
 
+/**
+ * Render a string that will represent the UI
+ * @param {string} appHtml - The entire React app as rendered by the server
+ * @param {object} jsVariables - a copy of the variables passed to the server.
+ * Used by client to render the React app
+ */
 function renderPage(appHtml, jsVariables = {}) {
   return `<!doctype html public="storage">
 <html>
@@ -56,15 +62,15 @@ module.exports = bsync((config) => {
 
   const app = new Koa();
 
-  // on 'error' is first in the koa middleware stack, should this be moved?
-  app.on(`error`, (error, ctx) => {
-    app.context.logger.error(`server error`, error, ctx);
-  });
-
   app.context.config = config;
   app.context.logger = logger;
 
   // Add middleware
+
+  // on 'error' is the first middleware in the koa middleware stack, should this be moved to later?
+  app.on(`error`, (error, ctx) => {
+    app.context.logger.error(`server error`, error, ctx);
+  });
   app.use(convert(cors()));
   app.use(convert(bodyparser()));
   app.use(convert(json()));
@@ -78,7 +84,12 @@ module.exports = bsync((config) => {
   const sequelize = new Sequelize(`postgres://${process.env.username}:${process.env.password}@localhost:5432/${process.env.dbname}`);
 
   // check database connection
-  const err = bwait(sequelize.authenticate());
+  let err;
+  try {
+    err = bwait(sequelize.authenticate());
+  } catch (ex) {
+    app.context.logger.error(`Sequelize authentication error`, ex);
+  }
 
   if (err) {
     const errorMessage = `Unable to connect to the database: ${err}`;
@@ -88,19 +99,30 @@ module.exports = bsync((config) => {
     app.context.db = sequelize;
   }
 
-
   // // Just wipe the database each time until we can smooth things out
   // app.context.db.sync({ force: true });
 
   // add custom middleware here
   const files = new Files(app, `/${config.apiVersion}/files`);
-  bwait(files.initialize());
+  try {
+    bwait(files.initialize());
+  } catch (ex) {
+    app.context.logger.error(`"Files" middleware initialization error`, ex);
+  }
 
   const jobs = new Jobs(app, `/${config.apiVersion}/jobs`);
-  bwait(jobs.initialize());
+  try {
+    bwait(jobs.initialize());
+  } catch (ex) {
+    app.context.logger.error(`"Jobs" middleware initialization error`, ex);
+  }
 
   const bots = new Bots(app, `/${config.apiVersion}/bots`);
-  bwait(bots.initialize());
+  try {
+    bwait(bots.initialize());
+  } catch (ex) {
+    app.context.logger.error(`"Bots" middleware initialization error`, ex);
+  }
 
   // app.context.db.sync({ force: true });
 
@@ -115,9 +137,15 @@ module.exports = bsync((config) => {
         } else if (redirect) {
           ctx.redirect(redirect.pathname + redirect.search);
           // Don't render anything if you are requesting an api url aka anything near "/v1"
-        } else if (ctx.req.headers && ctx.req.headers.referer && ctx.req.headers.referer.indexOf(`/v1`) !== -1) {
-          //
+        } else if (
+          ctx.req.headers &&
+          ctx.req.headers.referer &&
+          ctx.req.headers.referer.indexOf(`/v1`) !== -1
+        ) {
+          return;
         } else if (props) {
+          // Populate react variables to be passed to the client
+          // so that they match the variables used by server-side rendering
           const serverProps = {
             files: files.getFiles(),
             jobs: jobs.getJobs(),
