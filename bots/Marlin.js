6c6ee069291192e5f8edb9d6d9efea8a98f9ffdb
matthova <matthova87@gmail.com>
2016-09-21 12:21:09 -0700

    Added custom Checkpoint handling for Marlin

--- bots/Marlin.js
+++ bots/Marlin.js
@@ -1,4 +1,7 @@
+const Promise = require('bluebird');
 const _ = require('underscore');
+const LineByLineReader = Promise.promisifyAll(require('line-by-line'));
+const fs = require('fs');
 const bsync = require('asyncawait/async');
 const bwait = require('asyncawait/await');
 
@@ -8,12 +11,12 @@ const Marlin = function (app) {
   DefaultBot.call(this, app);
 
   _.extend(this.settings, {
-    name: `Marlin`,
+    name: 'Marlin',
     model: __filename.split(`${__dirname}/`)[1].split('.js')[0],
   });
 
   _.extend(this.info, {
-    connectionType: `serial`,
+    connectionType: 'serial',
     vid: undefined,
     pid: undefined,
     baudrate: undefined,
@@ -21,6 +24,134 @@ const Marlin = function (app) {
   });
 
   _.extend(this.commands, {
+    // In order to start processing a job, the job's file is opened and then
+    // processed one line at a time
+    startJob: bsync(function startJob(self, params) {
+      const job = params.job;
+      self.currentJob = job;
+      self.currentJob.checkpoint = null;
+      self.fsm.start();
+      const filesApp = self.app.context.files;
+      const theFile = filesApp.getFile(job.fileUuid);
+
+      // open the file
+      // start reading line by line...
+      self.lr = new LineByLineReader(theFile.filePath);
+      self.currentLine = 0;
+      bwait(self.lr.pause()); // redundant
+
+      self.lr.on('error', (err) => {
+        self.logger.error('line reader error:', err);
+      });
+
+      // As the buffer reads each line, process it
+      self.lr.on('line', bsync((line) => {
+        // pause the line reader immediately
+        // we will resume it as soon as the line is done processing
+        bwait(self.lr.pause());
+        // We only care about the info prior to the first semicolon
+        // NOTE This code is assuming we are processing GCODE
+        // In case of adding support for multiple control formats, this is a good place to start
+
+        // Looking for a comment between 3 brackets and whatever comes after that
+        const conductorComment = /^[\w\d\s]*; <<<(\w+)>>> (.*)$/;
+        const conductorCommentResult = conductorComment.exec(line);
+        if (conductorCommentResult !== null) {
+          switch (conductorCommentResult[1]) {
+            case 'CHECKPOINT': {
+              console.log('checkpoint', conductorCommentResult);
+              const botRegex = /^.*bot(\w+) : (\d+)$/;
+              const botAndCheckpoint = botRegex.exec(conductorCommentResult[2]);
+              // const bot = botAndCheckpoint[1];
+              const checkpoint = parseInt(botAndCheckpoint[2], 10);
+              self.status.checkpoint = parseInt(checkpoint, 10);
+              self.lr.resume();
+              // Let conductor know that you've reached the latest checkpoint
+              // Check if precursors are complete
+              break;
+            }
+            case 'PRECURSOR': {
+              console.log('precursor', conductorCommentResult);
+              const botRegex = /^.*(bot\w+) : (\d+)$/;
+              const botAndCheckpoint = botRegex.exec(conductorCommentResult[2]);
+              const bot = botAndCheckpoint[1];
+              const checkpoint = parseInt(botAndCheckpoint[2], 10);
+              console.log(bot, checkpoint);
+              self.status.blocker = { bot, checkpoint };
+              self.commands.checkPrecursors(self);
+              console.log('just hit a precursor', self.status);
+              break;
+            }
+            case 'DRY': {
+              console.log('dry', conductorCommentResult);
+              // jog in x, unpark
+              self.lr.resume();
+              break;
+            }
+            default: {
+              console.log('wuuuuh', conductorCommentResult);
+              break;
+            }
+          }
+        } else {
+          let command = line.split(';')[0];
+          if (command.length <= 0) {
+            // If the line is blank, move on to the next line
+            bwait(self.lr.resume());
+          } else {
+            command = self.addOffset(command);
+            command = self.addSpeedMultiplier(command);
+            command = self.addFeedMultiplier(command);
+
+            self.queue.queueCommands({
+              code: command,
+              postCallback: bsync(() => {
+                if (self.currentJob.fsm.current === 'running') {
+                  bwait(self.lr.resume());
+                }
+                self.currentLine += 1;
+                self.currentJob.percentComplete = parseInt((self.currentLine / self.numLines) * 100, 10);
+              }),
+            });
+          }
+        }
+      }));
+
+      self.lr.on('end', bsync(() => {
+        self.logger.info('completed reading file,', theFile.filePath, 'is closed now.');
+        bwait(self.lr.close());
+        self.queue.queueCommands({
+          postCallback: bsync(() => {
+            self.currentJob.percentComplete = 100;
+            bwait(self.fsm.stop());
+            bwait(self.fsm.stopDone());
+            bwait(self.currentJob.fsm.runningDone());
+            bwait(self.currentJob.stopwatch.stop());
+          }),
+        });
+      }));
+
+      // Get the number of lines in the file
+      let numLines = 0;
+      const fsPromise = new Promise((resolve, reject) => {
+        fs.createReadStream(theFile.filePath)
+        .on('data', function readStreamOnData(chunk) {
+          numLines += chunk
+          .toString('utf8')
+          .split(/\r\n|[\n\r\u0085\u2028\u2029]/g)
+          .length - 1;
+        })
+        .on('end', () => {  // done
+          self.numLines = numLines;
+          self.logger.info(`Bot will process file with ${self.numLines} lines.`);
+          resolve();
+        });
+      });
+
+      bwait(fsPromise);
+      self.lr.resume();
+      self.fsm.startDone();
+    }),
     updateRoutine: (self, params) => {
       self.status = {
         position: {
@@ -39,12 +170,18 @@ const Marlin = function (app) {
             setpoint: undefined,
           },
         },
+        checkpoint: self.status.checkpoint || undefined,
+        collaborators: self.status.collaborators || {},
+        blocker: self.status.blocker || {
+          bot: undefined,
+          checkpoint: undefined,
+        },
       };
 
-      if (self.fsm.current === `connected`) {
+      if (self.fsm.current === 'connected') {
         const commandArray = [];
         commandArray.push({
-          code: `M114`,
+          code: 'M114',
           processData: (command, reply) => {
             const newPosition = {
               x: undefined,
@@ -60,12 +197,12 @@ const Marlin = function (app) {
               self.status.position = newPosition;
               return true;
             } catch (ex) {
-              self.logger.error(`Failed to set position`, reply, ex);
+              self.logger.error('Failed to set position', reply, ex);
             }
           },
         });
         commandArray.push({
-          code: `M105`,
+          code: 'M105',
           processData: (command, reply) => {
             self.status.sensors.t0 = {
               temperature: '?',
@@ -80,19 +217,19 @@ const Marlin = function (app) {
               self.status.sensors.t0.temperature = reply.split('T:')[1].split(' ')[0];
               self.status.sensors.t0.setpoint = reply.split('T:')[1].split('/')[1].split(' ')[0];
             } catch (ex) {
-              // this.logger.info(`Failed to parse nozzle temp`);
+              // this.logger.info('Failed to parse nozzle temp');
             }
 
             try {
               self.status.sensors.b0.temperature = reply.split('B:')[1].split(' ')[0];
               self.status.sensors.b0.setpoint = reply.split('B:')[1].split('/')[1].split(' ')[0];
             } catch (ex) {
-              // this.logger.info(`Failed to parse bed temp`);
+              // this.logger.info('Failed to parse bed temp');
             }
 
-            self.app.io.broadcast(`botEvent`, {
+            self.app.io.broadcast('botEvent', {
               uuid: self.settings.uuid,
-              event: `update`,
+              event: 'update',
               data: self.getBot(),
             });
             return true;
@@ -104,7 +241,7 @@ const Marlin = function (app) {
     processGcode: bsync((self, params) => {
       const gcode = self.addOffset(params.gcode);
       if (gcode === undefined) {
-        throw `"gcode" is undefined`;
+        throw '"gcode" is undefined';
       }
       const commandArray = [];
 
@@ -113,7 +250,7 @@ const Marlin = function (app) {
         commandArray.push({
           code: gcode,
           processData: (command, reply) => {
-            resolve(reply.replace(`\r`, ``));
+            resolve(reply.replace('\r', ''));
             return true;
           },
         });
@@ -128,7 +265,7 @@ const Marlin = function (app) {
       }
       const gcode = self.addOffset(params.gcode);
       if (gcode === undefined) {
-        throw `"gcode" is undefined`;
+        throw '"gcode" is undefined';
       }
       const commandArray = [];
       commandArray.push(self.commands.gcodeInitialState(self, params));
@@ -142,7 +279,7 @@ const Marlin = function (app) {
       const commandArray = [];
       commandArray.push(self.commands.gcodeInitialState(self, params));
       commandArray.push({
-        code: `M114`,
+        code: 'M114',
         processData: (command, reply) => {
           const currentLocation = {};
           currentLocation.x = Number(reply.split('X:')[1].split('Y')[0]);
@@ -166,19 +303,19 @@ const Marlin = function (app) {
       return self.getBot();
     },
     gcodeInitialState: (self, params) => {
-      let command = ``;
+      let command = '';
       switch (self.fsm.current) {
-        case `connected`:
+        case 'connected':
           break;
-        case `processingJob`:
-        case `processingJobGcode`:
+        case 'processingJob':
+        case 'processingJobGcode':
           command = {
             preCallback: () => {
               self.fsm.jobToGcode();
             },
           };
           break;
-        case `parked`:
+        case 'parked':
           command = {
             preCallback: () => {
               self.fsm.parkToGcode();
@@ -191,19 +328,19 @@ const Marlin = function (app) {
       return command;
     },
     gcodeFinalState: (self, params) => {
-      let command = ``;
+      let command = '';
       switch (self.fsm.current) {
-        case `connected`:
+        case 'connected':
           break;
-        case `processingJob`:
-        case `processingJobGcode`:
+        case 'processingJob':
+        case 'processingJobGcode':
           command = {
             preCallback: () => {
               self.fsm.jobGcodeDone();
             },
           };
           break;
-        case `processingParkGcode`:
+        case 'processingParkGcode':
           command = {
             preCallback: () => {
               self.fsm.parkGcodeDone();
@@ -214,6 +351,18 @@ const Marlin = function (app) {
           break;
       }
       return command;
+    },
+    checkPrecursors: bsync(function checkPrecursors(self, params) {
+      console.log('current status', self.status);
+      if (self.status.collaborators[self.status.blocker.bot] >= self.status.blocker.checkpoint) {
+        self.lr.resume();
+      } else {
+        console.log('nope!', self.status);
+      }
+    }),
+    updateCollaboratorCheckpoints: (self, params) => {
+      self.status.collaborators = params.collaborators;
+      self.commands.checkPrecursors(self);
     },
   });
 };
