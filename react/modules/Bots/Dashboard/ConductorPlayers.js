021f057bbc54fc18fb947a3b1468e6378dbad082
matthova <matthova87@gmail.com>
2016-10-04 17:40:16 -0700

    adding new player form

--- react/modules/Bots/Dashboard/ConductorPlayers.js
+++ react/modules/Bots/Dashboard/ConductorPlayers.js
@@ -7,6 +7,14 @@ export default class ConductorPlayers extends React.Component {
     super(props);
   }
 
+  createPlayerList() {
+    const playerList = [];
+    for (const player of this.props.bot.settings.custom.players) {
+      playerList.push(this.createPlayer(player));
+    }
+    return playerList;
+  }
+
   createPlayer(player) {
     return (
       <div>
@@ -17,18 +25,33 @@ export default class ConductorPlayers extends React.Component {
     );
   }
 
-  createPlayerList() {
-    const playerList = [];
-    for (const player of this.props.bot.settings.custom.players) {
-      playerList.push(this.createPlayer(player));
-    }
-    return playerList;
+  addPlayer(event) {
+    event.preventDefault();
+  }
+
+  createNewPlayerForm() {
+    return (
+      <form onSubmit={this.addPlayer}>
+        <div>
+          <label htmlFor="name">Player Name:</label>
+          <input type="textarea" name="name" defaultValue="" />
+        </div>
+        <div>
+          <label htmlFor="endpoint">Player Endpoint:</label>
+          <input type="textarea" name="endpoint" defaultValue="" />
+        </div>
+        <input type="submit" value="Add Player" />
+      </form>
+    );
   }
 
   render() {
     const playerList = this.createPlayerList();
+    const newPlayerForm = this.createNewPlayerForm();
+
     return (
       <div>
+        {newPlayerForm}
         {playerList}
       </div>
     );
