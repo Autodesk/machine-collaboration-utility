bea54c990955c465ab93e4418df1140f5f8fb34e
rameezv <rameez@bigtreeworld.com>
2017-02-11 23:43:59 +0000

    Updated CamFeed model and added inputtable camera URL

--- react/modules/Bots/CamFeed/index.js
+++ react/modules/Bots/CamFeed/index.js
@@ -4,13 +4,38 @@ import request from 'superagent';
 export default class CamFeed extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {input:"http://0.0.0.0:8080/?action=stream", camUrl: "http://0.0.0.0:8080/?action=stream"};
+    this.updateUrl = this.updateUrl.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  updateUrl() {
+    this.setState(prevState => ({
+        camUrl: this.state.input
+    }));
+  }
+
+  handleInputChange(e) {
+    if (e.target.value !== null) {
+        this.setState(prevState => ({
+            input: e.target.value    
+        }));
+    }
+    e.persist()
   }
 
   render() {
     return (
       <div id="camfeed">
-        <img src="http://192.168.168.5:8080/?action=stream" alt="CamFeed" />
-      </div>
+        <div style={{width: '100%', textAlign: 'center'}}><img src={this.state.camUrl} alt="CamFeed" />
+        </div><div style={{width: '100%'}}>
+        <b>Camera URL:</b>
+        <br />
+        <input type="text" name="camUrlInput" id="camUrlInput" placeholder="Camera URL" value={this.state.input} onChange={this.handleInputChange} />
+        <a href="#" onClick={this.updateUrl}>Update</a>
+        </div>
+     </div>
     );
   }
+  
 }
