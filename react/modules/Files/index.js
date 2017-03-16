314323beef48c14c23d9c45f720f72c0988ff429
matthova <matthova87@gmail.com>
2017-03-16 10:34:52 -0700

    Fixed bug with kicking off a file to the wrong bot

--- react/modules/Files/index.js
+++ react/modules/Files/index.js
@@ -1,3 +1,4 @@
+/* global document */
 import React from 'react';
 import Modal from 'react-bootstrap/lib/Modal';
 import request from 'superagent';
@@ -9,13 +10,12 @@ export default class Files extends React.Component {
   constructor(props) {
     super(props);
 
-    // grab the first bot that is available to process a job
-    let initialBotUuid = undefined;
-
     this.state = {
       showModal: false,
-      botUuid: this.getActiveBotUuid(props),
+      fileUuid: undefined,
+      fileName: undefined,
     };
+
     this.handleProcessFile = this.handleProcessFile.bind(this);
     this.close = this.close.bind(this);
     this.startJob = this.startJob.bind(this);
@@ -39,32 +39,9 @@ export default class Files extends React.Component {
     this.setState({ botUuid });
   }
 
-  componentWillReceiveProps(newProps) {
-    this.setState({
-      botUuid: this.getActiveBotUuid(newProps),
-    });
-  }
-
-  getActiveBotUuid(props) {
-    // In case the botUuid is already set, just check to make sure that the bot is still connected
-    if (this.state && this.state.botUuid !== undefined) {
-      if (this.props.bots[this.state.botUuid].state === 'connected') {
-        return this.state.botUuid;
-      }
-    }
-    for (const [botUuid, bot] of _.pairs(this.props.bots)) {
-      // Only allow jobs to be stared on a bot in the state "connected"
-      if (bot.state !== 'connected') {
-        continue;
-      }
-      return botUuid;
-    }
-    return undefined;
-  }
-
   createBotList() {
     const options = [];
-    _.pairs(this.props.bots).map(([botUuid, bot]) => {
+    _.pairs(this.props.bots).forEach(([botUuid, bot]) => {
       // Only allow jobs to be stared on a bot in the state "connected"
       if (bot.state !== 'connected') {
         return;
@@ -73,9 +50,11 @@ export default class Files extends React.Component {
       // If the bot specifies an array of supported file fileTypes
       // Then make sure the selected file is consumable by the bot
       if (bot.info.fileTypes.length > 0) {
+        // Check if any of the supported files match the file we are about to process
         const supportedFileList = bot.info.fileTypes.filter(filetype => {
           return this.state.fileName && this.state.fileName.indexOf(filetype) !== -1;
         });
+        // If there's no match, don't add the bot to the option list
         if (supportedFileList.length <= 0) {
           return;
         }
@@ -83,18 +62,26 @@ export default class Files extends React.Component {
 
       options.push(<option key={botUuid} value={botUuid}>{bot.settings.name}</option>);
     });
+
     return (
-      <select name="botList" onChange={this.change} form="newJobForm">
+      <select id="process-file-modal" name="botList" onChange={this.change} form="newJobForm">
         {options}
       </select>
     );
   }
 
   startJob() {
+    // Grab the currently selected value from the select form
+    // If the value is undefined, or not a UUID, then ditch the "Start" sequence
+    const botUuid = document.getElementById('process-file-modal').value;
+    if (botUuid == undefined || botUuid.length !== 36) {
+      return;
+    }
+
     // Create a job
     const requestParams = {
       fileUuid: this.state.fileUuid,
-      botUuid: this.state.botUuid,
+      botUuid,
       startJob: true,
     };
 
@@ -107,20 +94,20 @@ export default class Files extends React.Component {
 
   renderModal() {
     return (
-    <Modal show={this.state.showModal} onHide={this.close}>
-      <Modal.Header closeButton>
-        <Modal.Title>Select bot to process file</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div>{this.state.fileUuid}</div>
-        <div>{this.state.fileName}</div>
-        {this.createBotList()}
-        <button onClick={this.startJob}>Start Job</button>
-      </Modal.Body>
-      <Modal.Footer>
-        <button onClick={this.close}>Close</button>
-      </Modal.Footer>
-    </Modal>
+      <Modal show={this.state.showModal} onHide={this.close}>
+        <Modal.Header closeButton>
+          <Modal.Title>Select bot to process file</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>{this.state.fileUuid}</div>
+          <div>{this.state.fileName}</div>
+          {this.createBotList()}
+          <button onClick={this.startJob}>Start Job</button>
+        </Modal.Body>
+        <Modal.Footer>
+          <button onClick={this.close}>Close</button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 
@@ -136,8 +123,11 @@ export default class Files extends React.Component {
       return <File key={file.uuid} file={file} handleProcessFile={this.handleProcessFile}/>;
     });
 
-    return (<div>
-      {this.renderModal()}
+    const modal = this.renderModal();
+
+    return (
+    <div>
+      {modal}
       <div id="files" className="container">
         <h1>Files</h1>
         <button className="upload" onClick={this.props.dropzoneOpener}>Upload File</button>
