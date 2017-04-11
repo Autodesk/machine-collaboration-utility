481b0e69a4a0f9310b9ac3fc7829889a0cbaec2e
matthova <matthova87@gmail.com>
2017-04-11 12:32:13 -0700

    Added Kloner3D bot

---
+++ bots/KLONER3D/index.js
@@ -0,0 +1,29 @@
+const _ = require('lodash');
+
+const VirtualBot = require('../Virtual');
+
+const info = {
+  connectionType: 'serial',
+  fileTypes: ['.gcode'],
+  vidPid: [
+    {
+      vid: 0x0403,
+      pid: 0x6001,
+    },
+  ],
+  baudrate: 115200,
+};
+
+const settings = {
+  name: 'KLONER3D',
+  model: __dirname.split('/')[__dirname.split('/').length - 1],
+};
+
+const commands = {
+};
+
+module.exports = {
+  info: _.extend(Object.assign({}, VirtualBot.info), info),
+  settings: _.extend(Object.assign({}, VirtualBot.settings), settings),
+  commands: _.extend(Object.assign({}, VirtualBot.commands), commands),
+};
