e8130a2ad67101ec33d13f7dd375808e783c5963
matthova <matthova87@gmail.com>
2017-04-11 17:19:59 -0700

    Deleted extraneous comments in the unpark command

--- bots/Virtual/commands/unpark.js
+++ bots/Virtual/commands/unpark.js
@@ -50,26 +50,3 @@ module.exports = async function unpark(self, params) {
   }
   return self.getBot();
 };
-
-// const delay = require('bluebird').delay;
-
-// module.exports = function unpark(self, params) {
-//   try {
-//     if (!(self.fsm.current === 'parked' || self.fsm.current === 'parking')) {
-//       throw new Error(`Cannot unpark from state "${self.fsm.current}"`);
-//     }
-
-//     const commandArray = [];
-//     commandArray.push({
-//       postCallback: async () => {
-//         self.fsm.unpark();
-//         await delay(1000);
-//         self.fsm.unparkDone();
-//       },
-//     });
-//     self.queue.queueCommands(commandArray);
-//   } catch (ex) {
-//     self.logger.error('Park fail', ex);
-//   }
-//   return self.getBot();
-// };
