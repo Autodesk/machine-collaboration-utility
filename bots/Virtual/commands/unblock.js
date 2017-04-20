7d08a07c12e9685a3d999c3b63baf9a3390408a2
matthova <matthova87@gmail.com>
2017-04-19 17:49:07 -0700

    Allow unpark when asked to unblock, even if already executing job

--- bots/Virtual/commands/unblock.js
+++ bots/Virtual/commands/unblock.js
@@ -4,47 +4,58 @@ const jobFsmDefinitions = require(path.join(process.env.PWD, 'react/modules/Jobs
 
 module.exports = async function unblock(self, params) {
   try {
-    if (!(self.fsm.current === 'blocked' || self.fsm.current === 'blocking')) {
-      throw new Error(`Cannot unblock from state "${self.fsm.current}"`);
-    }
-
-    const commandArray = [];
-
-    const unblockDoneCommand = {
-      postCallback: () => {
-        self.fsm.unblockDone();
-        self.lr.resume();
-      },
-    };
-
-    const unblockMotion = {
-      preCallback: () => {
-        self.logger.debug('Starting unblock motion');
-      },
-      delay: 1000,
-      postCallback: () => {
-        self.queue.prependCommands(unblockDoneCommand);
-        self.logger.debug('Done with unblock motion');
-      }
-    };
-
-    if (self.fsm.current === 'blocking') {
-      commandArray.push({
-        postCallback: () => {
-          self.fsm.unblock();
+    if (self.fsm.current === 'executingJob') {
+      const commandArray = [
+        // generateUnparkCommands(self),
+        {
+          postCallback: () => {
+            self.lr.resume();
+          }
         }
-      });
+      ]
+      self.queue.queueCommands(commandArray);
+    } else {
+      if (!(self.fsm.current === 'blocked' || self.fsm.current === 'blocking')) {
+        throw new Error(`Cannot unblock from state "${self.fsm.current}"`);
+      }
+
+      const commandArray = [];
+
+      const unblockDoneCommand = {
+        postCallback: () => {
+          self.fsm.unblockDone();
+          self.lr.resume();
+        },
+      };
+
+      const unblockMotion = {
+        preCallback: () => {
+          self.logger.debug('Starting unblock motion');
+        },
+        delay: 1000,
+        postCallback: () => {
+          self.queue.prependCommands(unblockDoneCommand);
+          self.logger.debug('Done with unblock motion');
+        }
+      };
+
+      if (self.fsm.current === 'blocking') {
+        commandArray.push({
+          postCallback: () => {
+            self.fsm.unblock();
+          }
+        });
+      }
+
+      commandArray.push(unblockMotion);
+      self.queue.queueCommands(commandArray);
+
+      // Queue the unblock command if currently 'blocking'
+      if (self.fsm.current === 'blocked') {
+        // Unblock the bot
+        self.fsm.unblock();
+      }
     }
-
-    commandArray.push(unblockMotion);
-    self.queue.queueCommands(commandArray);
-
-    // Queue the unblock command if currently 'blocking'
-    if (self.fsm.current === 'blocked') {
-      // Unblock the bot
-      self.fsm.unblock();
-    }
-
   } catch (ex) {
     self.logger.error('Unblock error', ex);
   }
