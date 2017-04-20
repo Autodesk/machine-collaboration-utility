61bbc2caf05ffc0a19f48ce74a72ea1f54da9910
matthova <matthova87@gmail.com>
2017-04-19 17:47:47 -0700

    Conductor now checks for cancel to be done on each bot

--- bots/Conductor/commands/cancel.js
+++ bots/Conductor/commands/cancel.js
@@ -3,6 +3,36 @@ const path = require('path');
 const botFsmDefinitions = require(path.join(process.env.PWD, 'react/modules/Bots/botFsmDefinitions'));
 const jobFsmDefinitions = require(path.join(process.env.PWD, 'react/modules/Jobs/jobFsmDefinitions'));
 
+async function checkCancel(self) {
+  // ping each bot
+  // If they're all caneled, then we are done canceling
+  let cancelDone = true;
+  await Promise.map(self.settings.custom.players, async (player) => {
+    const checkParams = {
+      method: 'GET',
+      uri: player.endpoint,
+      json: true,
+    };
+    const reply = await request(checkParams);
+    if (
+      botFsmDefinitions.metaStates.processingJob.includes(reply.data.state)
+      ||
+      reply.data.state === 'cancelingJob'
+    ) {
+      cancelDone = false;
+    }
+  });
+
+  if (cancelDone) {
+    self.currentJob.fsm.cancel();
+    self.currentJob = undefined;
+    self.fsm.cancelDone();
+  } else {
+    await Promise.delay(2000);
+    checkCancel(self);
+  }
+}
+
 
 module.exports = function cancel(self) {
   try {
@@ -17,28 +47,28 @@ module.exports = function cancel(self) {
     }
 
     self.fsm.cancel();
+    try {
+      const players = self.settings.custom.players;
+      players.forEach(async (player) => {
+          // Cancel each bot
+        const cancelJobParams = {
+          method: 'POST',
+          uri: player.endpoint,
+          body: {
+            command: 'cancel',
+          },
+          json: true,
+        };
 
-    const players = self.settings.custom.players;
-    players.forEach(async (player) => {
-        // Cancel each bot
-      const cancelJobParams = {
-        method: 'POST',
-        uri: player.endpoint,
-        body: {
-          command: 'cancel',
-        },
-        json: true,
-      };
-
-      await request(cancelJobParams)
-      .catch(ex => {
-        self.logger.error('Cancel fail', ex);
+        await request(cancelJobParams)
+        .catch(ex => {
+          self.logger.error('Cancel fail', ex);
+        });
       });
-    });
-
-    self.currentJob.fsm.cancel();
-    self.currentJob = undefined;
-    self.fsm.cancelDone();
+      checkCancel(self);
+    } catch (ex) {
+      self.logger.error('Check cancel error', ex);
+    }
   } catch (ex) {
     self.logger.error(ex);
   }
