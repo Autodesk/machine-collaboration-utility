7e3ec463323044559bf91eff30dd8f8a4cd748c4
matthova <matthova87@gmail.com>
2017-08-31 15:58:43 -0700

    added ability to force gcode consumption while processing a job, if a flag is passed

--- bots/Virtual/commands/processGcode.js
+++ bots/Virtual/commands/processGcode.js
@@ -2,11 +2,15 @@
 const gcodeToObject = require('gcode-json-converter').gcodeToObject;
 const objectToGcode = require('gcode-json-converter').objectToGcode;
 
+// Param inputs
+// @gcode: <string> A string of gcode to be executed
+// @force: <boolean> Force gcode execution even if the bot is processing a job
 module.exports = async function processGcode(self, params) {
   try {
     if (
       self.fsm.current !== 'paused'
       && self.fsm.current !== 'idle'
+      && params.force !== true
     ) {
       throw new Error(`Cannot process gcode from state ${self.fsm.current}`);
     }
