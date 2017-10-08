f7bc1664091c3716421a4776ae5c09f7a40022b0
hovanem <matt.hova@autodesk.com>
2017-10-08 12:42:22 -0500

    removed extraneous files and package references

--- install_os_dependencies.js
+++ install_os_dependencies.js
@@ -1,4 +1,4 @@
-const exec = require("child_process").exec;
+const exec = require('child_process').exec;
 
 let hardwareDependencies = null;
 
@@ -8,16 +8,16 @@ if (process.env.CONTINUOUS_INTEGRATION !== 'true') {
 
 if (hardwareDependencies) {
   for (var package in hardwareDependencies.dependencies) {
-    var oscmd   = "npm install " + package;
+    var oscmd = 'npm install ' + package;
     exec(oscmd, function(error, stdout, stderr) {
-      if (error)  {
-        console.log("ERROR:", error);
+      if (error) {
+        console.log('ERROR:', error);
       }
       if (stdout) {
-        console.log( stdout );
+        console.log(stdout);
       }
       if (stderr) {
-        console.log( stderr );
+        console.log(stderr);
       }
     });
   }
