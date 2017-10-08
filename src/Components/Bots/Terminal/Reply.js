9bdab00494abee1adfdccc1bee2445095c2282d4
hovanem <matt.hova@autodesk.com>
2017-10-08 11:07:29 -0500

    initial terminal styles

--- src/Components/Bots/Terminal/Reply.js
+++ src/Components/Bots/Terminal/Reply.js
@@ -4,9 +4,18 @@ import moment from 'moment';
 class Reply extends React.Component {
   render() {
     const timestamp = new moment(this.props.reply.time);
-    return <div>
-      <p>{this.props.reply.sent ? 'TX' : 'RX'}: {timestamp.format('HH:mm:ss')}: {this.props.reply.command}</p>
+    return (
+      <div>
+        <p>
+          <span
+            className={this.props.reply.sent ? 'terminal__message--tx' : 'terminal__message--rx'}
+          >
+            {this.props.reply.sent ? 'TX' : 'RX'}
+          </span>: <span className="">{timestamp.format('HH:mm:ss')}</span>:{' '}
+          <span className="terminal__message--content">{this.props.reply.command}</span>
+        </p>
       </div>
+    );
   }
 }
 
