73ff275b395ff931dfa60a2a25bba05c0332646d
hovanem <matt.hova@autodesk.com>
2017-10-09 15:56:22 -0700

    Breaking up reply into multiple lines

--- src/Components/Bots/Terminal/Reply.js
+++ src/Components/Bots/Terminal/Reply.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import moment from 'moment';
 
+const Line = props => <div>{props.line}</div>;
+
 class Reply extends React.Component {
+  breakUpLines(command) {
+    return command.split('\n').map((line, i) => <Line key={i} line={line} />);
+  }
+
   render() {
     const timestamp = new moment(this.props.reply.time);
     return (
       <div>
-        <p>
+        <div>
           <span
             className={this.props.reply.sent ? 'terminal__message--tx' : 'terminal__message--rx'}
           >
             {this.props.reply.sent ? 'TX' : 'RX'}
           </span>: <span className="">{timestamp.format('HH:mm:ss')}</span>:{' '}
-          <span className="terminal__message--content">{this.props.reply.command}</span>
-        </p>
+          <span className="terminal__message--content">
+            {this.breakUpLines(this.props.reply.command)}
+          </span>
+        </div>
       </div>
     );
   }
