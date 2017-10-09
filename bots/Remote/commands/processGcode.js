d6464d15f43f7e784b2f0e270637616ed9d6e065
hovanem <matt.hova@autodesk.com>
2017-10-09 15:48:22 -0700

    Broadcast sent gcode to remote terminal

--- bots/Remote/commands/processGcode.js
+++ bots/Remote/commands/processGcode.js
@@ -1,12 +1,24 @@
 /* global logger */
-const request = require('request');
+const request = require('request-promise');
 
 // Param inputs
 // @gcode: <string> A string of gcode to be executed
 // @force: <boolean> Force gcode execution even if the bot is processing a job
 module.exports = async function processGcode(self, params) {
   try {
-    await request(self.settings.endpoint, { method: 'POST', json: true, body: params });
+    if (params.gcode) {
+      self.app.io.broadcast(`botTx${self.settings.uuid}`, params.gcode);
+    }
+
+    const reply = await request(self.settings.endpoint, {
+      method: 'POST',
+      json: true,
+      body: params,
+    });
+
+    if (reply.data) {
+      self.app.io.broadcast(`botRx${self.settings.uuid}`, reply.data);
+    }
   } catch (ex) {
     logger.error(ex);
   }
