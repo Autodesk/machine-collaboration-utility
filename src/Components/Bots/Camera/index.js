186d0a90975571fda41d7c46f0b6c7c9ee23cc5f
hovanem <matt.hova@autodesk.com>
2017-10-09 18:27:00 -0700

    fixed url resolution for the camera endpoint

--- src/Components/Bots/Camera/index.js
+++ src/Components/Bots/Camera/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Camera extends React.Component {
   render() {
-    let cameraEndpoint = 'http://localhost:8090?action=stream';
+    let cameraEndpoint = `${window.location.split('/#')[0]}8090?action=stream`;
     if (this.props.bot.settings.model === 'Remote') {
       cameraEndpoint = `${this.props.bot.settings.endpoint.split('/v1')[0]}:8090?action=stream`;
       // TODO add handling if remote mcu hosted at port other than 80
