304d55e7b18ec20791c0a66b8ad026ee192898f5
hovanem <matt.hova@autodesk.com>
2017-10-26 17:00:59 -0700

    Slimmed down position feedback

--- src/Components/Bots/Dashboard/PositionFeedback.js
+++ src/Components/Bots/Dashboard/PositionFeedback.js
@@ -1,43 +1,21 @@
 import React from 'react';
-import _ from 'lodash';
 
-import HoverAndClick from './HoverAndClick';
+const Axis = props => (
+  <div className="col-xs-3 no-padding">
+    <center>
+      <div>
+        {props.axis.toUpperCase()}: <span className="positioning--text">{props.position}</span>
+      </div>
+    </center>
+  </div>
+);
 
 export default class PositionFeedback extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.zeroAxis = this.zeroAxis.bind(this);
-  }
-
-  zeroAxis(inputAxes) {
-    const axes = { x: false, y: false, z: false };
-    _.extend(axes, inputAxes);
-    let gcode = '';
-    if (axes.x) {
-      gcode = 'G92 X0';
-    } else if (axes.y) {
-      gcode = 'G92 Y0';
-    } else if (axes.z) {
-      gcode = 'G92 Z0';
-    }
-
-    const commandObject = {
-      botUuid: this.props.endpoint,
-      command: 'processGcode',
-      gcode,
-    };
-
-    this.props.client.emit('command', commandObject);
-  }
-
   // Round the number. If no number, return a '-'
   parseNumber(number) {
     return Number.isNaN(Number(number)) ? '-' : Number(number).toFixed(3);
   }
   render() {
-    const zeroable = this.props.bot.state === 'idle' || this.props.bot.state === 'paused';
-
     const position =
       this.props.bot.status && this.props.bot.status.position
         ? this.props.bot.status.position
@@ -45,48 +23,14 @@ export default class PositionFeedback extends React.Component {
 
     return (
       <div className="positioning">
-        <h3>POSITIONING</h3>
-        <div className="col-xs-4 no-padding">
-          <h5>X-Axis</h5>
-          <div>{this.parseNumber(position.x)}</div>
-          <HoverAndClick color={{ h: this.props.appColor, s: zeroable ? 40 : 5, l: 40 }}>
-            <button
-              disabled={!zeroable}
-              onClick={() => {
-                this.zeroAxis({ x: true });
-              }}
-            >
-              Zero X
-            </button>
-          </HoverAndClick>
+        <div>
+          <h3>POSITION</h3>
         </div>
-        <div className="col-xs-4 no-padding">
-          <h5>Y-Axis</h5>
-          <div>{this.parseNumber(position.y)}</div>
-          <HoverAndClick color={{ h: this.props.appColor, s: zeroable ? 40 : 5, l: 40 }}>
-            <button
-              disabled={!zeroable}
-              onClick={() => {
-                this.zeroAxis({ y: true });
-              }}
-            >
-              Zero Y
-            </button>
-          </HoverAndClick>
-        </div>
-        <div className="col-xs-4 no-padding">
-          <h5>Z-Axis</h5>
-          <div>{this.parseNumber(position.z)}</div>
-          <HoverAndClick color={{ h: this.props.appColor, s: zeroable ? 40 : 5, l: 40 }}>
-            <button
-              disabled={!zeroable}
-              onClick={() => {
-                this.zeroAxis({ z: true });
-              }}
-            >
-              Zero Z
-            </button>
-          </HoverAndClick>
+        <div className="row">
+          <Axis axis="x" position={this.parseNumber(position.x)} />
+          <Axis axis="y" position={this.parseNumber(position.y)} />
+          <Axis axis="z" position={this.parseNumber(position.z)} />
+          <Axis axis="e" position={this.parseNumber(position.e)} />
         </div>
       </div>
     );
