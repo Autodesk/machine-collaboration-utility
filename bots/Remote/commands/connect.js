53281b27bdf2a236aa7f9027d3fec68d94dbd08d
hovanem <matt.hova@autodesk.com>
2017-11-08 14:02:41 -0800

    Use the request-promise library. If the request fails, revert to the old state

--- bots/Remote/commands/connect.js
+++ bots/Remote/commands/connect.js
@@ -1,6 +1,7 @@
 /* global logger */
 
-const request = require('request');
+const request = require('request-promise');
+const { delay } = require('bluebird');
 
 module.exports = async function connect(self) {
   try {
@@ -10,10 +11,21 @@ module.exports = async function connect(self) {
       event: 'update',
       data: self.getBot(),
     });
+
     await request(self.settings.endpoint, {
       method: 'POST',
       json: true,
       body: { command: 'connect' },
+    }).catch(async (ex) => {
+      // If connect command fails, set bot back to idle
+      // Wait 500ms before sending to prevent confusion from near-instanteous UI change
+      await delay(500);
+      self.fsm.current = 'ready';
+      self.app.io.broadcast('botEvent', {
+        uuid: self.settings.uuid,
+        event: 'update',
+        data: self.getBot(),
+      });
     });
   } catch (ex) {
     logger.error('Connect fail', ex);
