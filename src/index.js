a386f7a752ba2abc959633f0e146a779acc28eea
hovanem <matt.hova@autodesk.com>
2017-11-08 13:53:08 -0800

    Added back the service worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import './main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
